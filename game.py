d832ead950949bcaa8989ae6fb6d551cf246fca7
Ben Wurster <wursterben17@gmail.com>
2022-02-17 21:17:31 -0600

    basic hand tracking

--- game.py
+++ game.py
@@ -15,15 +15,13 @@ while True:
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             h, w, c = img.shape
-            x1, x2 = int(lm.x * w), int(lm.y * h)
-            for id, lm in enumerate(handLms.landmark):
-                h, w, c = img.shape
-                p1 = (int(handLms.landmark[0].x * w), int(handLms.landmark[0].y * h))
-                p2 = (int(handLms.landmark[5].x * w), int(handLms.landmark[5].y * h))
-                p3 = (int(handLms.landmark[17].x * w), int(handLms.landmark[17].y * h))
-                cv2.circle(img, p1, 10, (255, 0, 0), 2)
-                cv2.circle(img, p2, 10, (255, 0, 0), 2)
-                cv2.circle(img, p3, 10, (255, 0, 0), 2)
+            p1 = (int(handLms.landmark[0].x * w), int(handLms.landmark[0].y * h))
+            p2 = (int(handLms.landmark[5].x * w), int(handLms.landmark[5].y * h))
+            p3 = (int(handLms.landmark[17].x * w), int(handLms.landmark[17].y * h))
+            
+            cent = (int((p1[0] + p2[0] + p3[0])/3), int((p1[1] + p2[1] + p3[1])/3))
+
+            cv2.circle(img, cent, 30, (255, 0, 0), cv2.FILLED, 2)
 
     cv2.imshow("Image", img)
     k = cv2.waitKey(1) & 0xFF
